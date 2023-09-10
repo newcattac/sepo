600bddc12375a61bf3192517feae617eec93701f
newcattac <142712816+newcattac@users.noreply.github.com>
2023-09-10 20:24:18 +0900

    그럴싸 해보임

---
+++ download/es.js
@@ -0,0 +1,55 @@
+document.getElementById("1downloadButton").addEventListener("click", function() {
+    // 다운로드할 파일의 경로와 파일명 설정
+    var fileURL = "SEPo-PAPPEr0.0.exe";
+    var fileName = "SEPo-PAPPEr0.0.exe";
+
+    // 새로운 a 태그를 생성하여 다운로드 링크 생성
+    var a = document.createElement("a");
+    a.href = fileURL;
+    a.download = fileName;
+
+    // a 태그를 클릭하여 파일 다운로드 실행
+    a.click();
+});
+
+document.getElementById("2downloadButton").addEventListener("click", function() {
+    // 다운로드할 파일의 경로와 파일명 설정
+    var fileURL = "SEPo-PAPPEr0.0s.exe";
+    var fileName = "SEPo-PAPPEr0.0s.exe";
+
+    // 새로운 a 태그를 생성하여 다운로드 링크 생성
+    var a = document.createElement("a");
+    a.href = fileURL;
+    a.download = fileName;
+
+    // a 태그를 클릭하여 파일 다운로드 실행
+    a.click();
+});
+
+document.getElementById("3downloadButton").addEventListener("click", function() {
+    // 다운로드할 파일의 경로와 파일명 설정
+    var fileURL = "SEPo-PAPPEr0.0ui.exe";
+    var fileName = "SEPo-PAPPEr0.0ui.exe";
+
+    // 새로운 a 태그를 생성하여 다운로드 링크 생성
+    var a = document.createElement("a");
+    a.href = fileURL;
+    a.download = fileName;
+
+    // a 태그를 클릭하여 파일 다운로드 실행
+    a.click();
+});
+
+document.getElementById("4downloadButton").addEventListener("click", function() {
+    // 다운로드할 파일의 경로와 파일명 설정
+    var fileURL = "SEPo-PAPPEr0.1a.exe";
+    var fileName = "SEPo-PAPPEr0.1a.exe";
+
+    // 새로운 a 태그를 생성하여 다운로드 링크 생성
+    var a = document.createElement("a");
+    a.href = fileURL;
+    a.download = fileName;
+
+    // a 태그를 클릭하여 파일 다운로드 실행
+    a.click();
+});
